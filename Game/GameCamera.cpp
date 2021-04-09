df7afd66a634c55c2ed00da9963f0b4dfde98db2
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2021-04-09 12:18:45 +0900

    カメラを修正した。

--- Game/GameCamera.cpp
+++ Game/GameCamera.cpp
@@ -13,7 +13,7 @@ GameCamera::~GameCamera()
 bool GameCamera::Start()
 {
 	//注視点から視点までのベクトルを設定。
-	m_toCameraPos.Set(0.0f, 125.0f, 250.0f);
+	m_toCameraPos.Set(0.0f, 125.0f, -250.0f);
 	//プレイヤーのインスタンスを探す。
 	m_player = FindGO<Player>("player");
 
