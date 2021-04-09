ac360d9c5c8426fd8a148e364294f658f1c5fc9e
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2021-04-09 16:01:49 +0900

    当たり判定を描画しないように

--- Game/BackGround.cpp
+++ Game/BackGround.cpp
@@ -9,7 +9,7 @@ BackGround::BackGround()
 	modelRender.Update();
 	//Collider(‚ ‚½‚è”»’è)‚ðì‚éB
 	physicsStaticObject.CreateFromModel(modelRender.GetModel(), modelRender.GetModel().GetWorldMatrix());
-	PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
+	//PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
 }
 
 BackGround::~BackGround()
